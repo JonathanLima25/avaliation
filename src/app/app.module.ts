d64bc708d6bd8285285379e74d74c0bdbcb1fe30
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2021-07-06 11:08:14 -0300

    Implementando modulo funcionarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FuncionariosModule } from './funcionarios'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FuncionariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
